5b949d634a60da6b267febaa2127286ab80af487
nikita24383 <55633694+nikita24383@users.noreply.github.com>
2020-09-08 11:38:49 +0530

    Update and rename login.pnp

--- login.php
+++ login.php
@@ -1,23 +1,21 @@
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <!DOCTYPE html>
-<!-- Created By CodingNepal -->
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Login Form Design | CodeLab</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Planner | Login</title>
     <link rel="stylesheet" href="css/login.css">
   </head>
   <body>
     <div class="wrapper">
-      <div class="title">
-Login Form</div>
-<form action="#">
+      <div class="title">Login Form</div>
+      <form action="validation.php" method="post">
         <div class="field">
-          <input type="text" required>
+          <input type="text" name="username" required>
           <label>Username</label>
         </div>
-<div class="field">
-          <input type="password" required>
+        <div class="field">
+          <input type="password" name="password" required>
           <label>Password</label>
         </div>
 <!-- <div class="content">
@@ -28,11 +26,8 @@ Login Form</div>
 <div class="pass-link">
 <a href="#">Forgot password?</a></div>
 </div> -->
-<div class="field" id="log">
-          <input type="submit" value="LOGIN">
-        </div>
-<!-- <div class="signup-link">
-Not a member? <a href="#">Signup now</a></div> -->
+        <div class="field" id="log"><input type="submit" value="LOGIN"></div>
+        <div class="signup-link">Not a member? <a href="Register.php">Signup now</a></div>
 </form>
 </div>
 </body>
